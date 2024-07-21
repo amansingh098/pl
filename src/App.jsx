46de5f7229e90ab1c1a04016d2a56df0750f2093
Anisha ji <20@eg106202@anurag.edu.in>
2024-07-22 01:19:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Welcome from './components/Welcome';
+import './components/Welcome.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Welcome />
+    </div>
+  );
+};
+
+export default App;
